517cd809f301b5b87f4e9b61279fe4f45ff8af70
zsykk <shaoya.zhang01@estudiant.upf.edu>
2026-05-17 16:29:22 +0200

    add print in class_weights to make sure

--- src/fer_project/data.py
+++ src/fer_project/data.py
@@ -174,6 +174,7 @@ def class_weights(labels: np.ndarray, num_classes: int = 7) -> torch.Tensor:
         Float tensor of per-class weights suitable for cross-entropy loss.
     """
     counts = np.bincount(labels, minlength=num_classes)
+    print("USING SQRT WEIGHTS")
     # weights = counts.sum() / np.maximum(counts, 1)
     weights = 1.0 / np.sqrt(np.maximum(counts, 1))
     weights = weights / weights.mean()
